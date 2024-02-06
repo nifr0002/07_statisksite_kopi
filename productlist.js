18ba000e7719deabc30b5ebf8386541cdf471d99
Nicoline <nifr0002@stud.kea.dk>
2024-02-06 18:03:01 +0100

    nyt html produktliste

--- productlist.js
+++ productlist.js
@@ -1,35 +1,26 @@
 fetch("https://kea-alt-del.dk/t7/api/products")
-.then(res=>res.json())
-.then(showProducts)
+  .then((res) => res.json())
+  .then(showProducts);
 
-function showProducts(products){
-    //looper og kalder showProduct
-    products.forEach(showProduct)
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
 }
 
-function showProduct(product){
-    console.log(product);
-//fanf template
-const template = document.querySelector("product-cardTemplate").content;
+function showProduct(product) {
+  console.log(product);
+  //fang template
+  const template = document.querySelector("product-cardTemplate").content;
 
-//Lav en kopi 
- const copy= template.cloneNode (true);
+  //Lav en kopi
+  const copy = template.cloneNode(true);
 
-//ændre indhold
-copy.querySelector ("h2").textContent=product.productdisplayname;
-if(product.soldout){
+  //ændre indhold
+  copy.querySelector("h2").textContent = product.productdisplayname;
+  if (product.soldout) {
     //produktet er udsolgt
     copy.querySelector("article").classList.add(soldout);
+  }
+  //appende
+  document.querySelector("main").appendChild(copy);
 }
-//apende
-document.querySelector("main").appendChild(copy)
-}
-
-
-/*<div class="product-card">
-                <img src="https://kea-alt-del.dk/t7/images/webp/640/1573.webp" alt="Produktbillede">
-                <h2>Leggings</h2>
-                <p>Pris: 300 kr</p>
-                <p class="discounted-price">Rabatpris: 250 kr</p>
-                <a href="produkt.html">Se detaljer</a>
-            </div>*/
